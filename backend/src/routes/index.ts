d285dea1707f6bb8f06ec2aeac6a27b106fdce12
Elvin Sanchez <emsanchez891@gmail.com>
2022-01-28 09:07:25 -0600

    adding routes prizes

--- backend/src/routes/index.ts
+++ backend/src/routes/index.ts
@@ -1,10 +1,12 @@
 import { Router } from 'express';
 import user from './user.routes';
 import session from './session.routes';
+import prizes from './prizes.routes';
 
 const router = Router();
 
 router.use(user);
 router.use(session);
+router.use(prizes);
 
 export default router;
